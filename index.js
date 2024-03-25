ca58ab211d35ebe0abfadb52014644b6c7eeebb2
bob <Mansalati18@gmail.com>
2024-03-25 10:58:21 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [
+    { id: 1, quantity: 4 },
+    { id: 2, quantity: 5 },
+    { id: 3, quantity: 3 },
+    { id: 4, quantity: 6 },
+    { id: 5, quantity: 8 }
+];
 
-// Code your solution here
+const totalBatteries = batteryBatches.reduce((total, batch) => {
+    return total + batch.quantity;
+}, 0);
+
+console.log(totalBatteries); 
